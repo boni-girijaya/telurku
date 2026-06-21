7b2dc71044e1e5308910db7c9c88ebdba881a31a
boni-girijaya <system.girijaya@gmail.com>
2026-06-21 23:46:35 +0700

    Add files via upload
    Update Supabase config

--- telurku/supabase-config.js
+++ telurku/supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = "https://msguumnynnzlwwejlffn.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_a2hZ6Uv2yPSmVZW6BebEuA_spqDsH_g";
+export const SUPABASE_URL = "https://hwdmjqoosflswvewujrt.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_9_GGWDDG6DhftLaOAaXYig_g7pT-x9C";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
